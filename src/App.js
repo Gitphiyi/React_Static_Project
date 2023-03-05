56f9b0de43d0770ca3aa028d4b0a7679ff91ce9a
Philip Yi <philip.yi@duke.edu>
2023-03-04 22:22:17 -0500

    start of webpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import profilepic from './LinkedInPic.jpg'
 
-function App() {
+const NavBar = () => {
+  return(
+    <div className = 'Navbar'>
+      <h1 className ='Header'>PY</h1>
+      <div className='Header_Container'>
+        
+          <div className='List'> Home </div>
+          <div className='List'> About Me</div>
+          <div className='List'> Resume </div>
+          <div className='List'> Projects</div>
+        
+      </div>
+    </div>
+
+  )
+}
+const Introduction = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='Intro'>
+      <div className='Intro_Words'>
+        <h4>Hello! My name is</h4>
+        <h1>Philip Yi</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I am a student at Duke University majoring in Electrical & Computer Engineering
+          and Computer Science. I am interested in Robotics, Full-Stack Development, Machine Learning, 
+          and many other topics!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      
+      <div className='profilePic'>
+        <img src = {profilepic} alt = "picture of me!" className='profilePic'></img>
+      </div>
     </div>
   );
 }
+const App = () => {
+  return(
+    <div className='App'>
+      <NavBar />
+      <Introduction />
+    </div>
+  )
+}
+
 
 export default App;
